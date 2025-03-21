60bd1c7dbd59701bf5c0af35018ce5e938f1e9e8
Rkoester47 <koestr@rpi.edu>
2025-03-21 16:08:48 -0400

    World Creator UI
    Changes to creating new worlds

--- Assets/Scripts/Scrpt_UI/Scrpt_WorldCreator/NewWorldUi.cs
+++ Assets/Scripts/Scrpt_UI/Scrpt_WorldCreator/NewWorldUi.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 
 public class NewWorldUi : MonoBehaviour
 {
-    
+    static DataService dataservice = null;
+
+
     void Start()
     {
         GameObject datamgnt = GameObject.Find("DataMgnt");
@@ -15,9 +17,15 @@ public class NewWorldUi : MonoBehaviour
         print(info); //debugging print statement
 
     }
-    static void CreateWorldButton(string name, string path )
+    static void CreateWorldButton(string name)
     {
+        //load into world before saving
 
+        //save game after creating world
+        if (dataservice != null){ 
+            string savePath = dataservice.GetSavePath() + name.ToLower();
+            dataservice.Save(savePath);
+        }
     }
 
     void Update()
