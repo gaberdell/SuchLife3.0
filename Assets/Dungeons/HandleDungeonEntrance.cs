660a58abfdd129d4b7e8dafcd13745e4c49b1180
colekrushel <krushc@rpi.edu>
2025-03-23 11:40:37 -0400

    dungeon entrance/exit now triggers on interact key

--- Assets/Dungeons/HandleDungeonEntrance.cs
+++ Assets/Dungeons/HandleDungeonEntrance.cs
@@ -4,31 +4,38 @@ public class HandleDungeonEntrance : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     InputHandler inputHandler;
+    bool interact;
+    bool playerInRange;
     private void Start()
     {
         inputHandler = InputHandler.Instance;
+        interact = false;
+        playerInRange = false;
     }
     private void Update()
     {
-        bool interact = inputHandler.InteractTriggered;
-        //Debug.Log(interact);
-        if(interact){
+
+        //trigger on true -> false (when key is let go)
+        if(interact == true && inputHandler.InteractTriggered == false && playerInRange == true){
             Debug.Log("eeeee");
+            //generate dungeon if none exists 
+            GameObject dungeonGrid = GameObject.Find("DungeonGrid");
+            dungeonGrid.GetComponent<RenderDungeon>().StartRender(gameObject);
+            //gameObject.SetActive(false);
+            //move player to starting room in dungeon (0,0 point)
+            GameObject.Find("Player").transform.position = dungeonGrid.transform.position + new Vector3Int(5, 5, 0);
         }
+        interact = inputHandler.InteractTriggered;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //generate dungeon if none exists 
-        Debug.Log("interact triggered");
-        GameObject dungeonGrid = GameObject.Find("DungeonGrid");
-        dungeonGrid.GetComponent<RenderDungeon>().StartRender(gameObject);
-        //newDungeon.StartRender();
-        //gameObject.SetActive(false);
-        //move player to starting room in dungeon (0,0 point)
-        GameObject.Find("Player").transform.position = dungeonGrid.transform.position + new Vector3Int(5, 5, 0);
+       playerInRange = true;
     }
-
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       playerInRange = false;
+    }
 
 
 }
--- Assets/Dungeons/HandleDungeonExit.cs
+++ Assets/Dungeons/HandleDungeonExit.cs
@@ -3,19 +3,37 @@ using UnityEngine;
 public class HandleDungeonExit : MonoBehaviour
 {
     GameObject entrance;
+    InputHandler inputHandler;
+    bool interact;
+    bool playerInRange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void setEntrance(GameObject entrance)
     {
         this.entrance = entrance;
+        inputHandler = InputHandler.Instance;
+        interact = false;
+        playerInRange = false;
+    }
+
+    private void Update()
+    {
+        //trigger on true -> false (when key is let go)
+        if (interact == true && inputHandler.InteractTriggered == false && playerInRange == true)
+        {
+            //move player back to entrance
+            //newDungeon.StartRender();
+            //gameObject.SetActive(false);
+            GameObject.Find("Player").transform.position = entrance.transform.position;
+        }
+        interact = inputHandler.InteractTriggered;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //move player back to entrance
-        Debug.Log("interact triggered");
-        //newDungeon.StartRender();
-        //gameObject.SetActive(false);
-        //move player to starting room in dungeon (0,0 point)
-        GameObject.Find("Player").transform.position = entrance.transform.position; 
+        playerInRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInRange = false;
     }
 }
