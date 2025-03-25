379286ba7ddabd9128e96ac7473fc27b6b26608f
nickb14 <nbilsborrow@gmail.com>
2025-03-25 17:11:39 -0400

    graph updates with dungeon creation

--- Assets/Scripts/Scrpt_Astar/GraphUpdater.cs
+++ Assets/Scripts/Scrpt_Astar/GraphUpdater.cs
@@ -4,18 +4,38 @@ using UnityEngine.Tilemaps;
 
 public class GraphUpdater : MonoBehaviour
 {
+    public bool jankScanGraphEveryTime;
+    bool dungeonCreated = false;
+
     void OnEnable()
     {
+        EventManager.PlayerEnterDungeon += OnDungeonCreated;
         Tilemap.tilemapTileChanged += OnTilemapChanged;
     }
 
     void OnDisable()
     {
+        EventManager.PlayerEnterDungeon -= OnDungeonCreated;
         Tilemap.tilemapTileChanged -= OnTilemapChanged;
+    }
+
+    void OnDungeonCreated()
+    {
+        Invoke("InitialScan", 1);
+        
+        Debug.Log("TILEMAP CHANGED");
+    }
+
+    void InitialScan()
+    {
+        AstarPath.active.Scan();
+        dungeonCreated = true;
     }
 
     void OnTilemapChanged(Tilemap tilemap, Tilemap.SyncTile[] arg2)
     {
-        AstarPath.active.Scan();
+        if (dungeonCreated && jankScanGraphEveryTime)
+            AstarPath.active.Scan();
     }
+
 }
