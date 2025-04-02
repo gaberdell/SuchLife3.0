50e0180d086139869962b932a437867a195d94cc
Jason Kim <jasonkim050@gmail.com>
2025-04-01 22:42:51 -0400

    Split Save() into NewSave() and SaveCurr()

--- Assets/Scripts/Scrpt_SaveLoad/DataService.cs
+++ Assets/Scripts/Scrpt_SaveLoad/DataService.cs
@@ -1,3 +1,4 @@
+using Codice.CM.Client.Differences;
 using JetBrains.Annotations;
 using NUnit.Framework;
 using System;
@@ -6,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 using UnityEngine.UIElements.Experimental;
 
 
@@ -20,7 +22,7 @@ public class DataService {
   private static string savePath = System.IO.Directory.GetCurrentDirectory() + "\\Saves\\";
   private static int savePathLen = savePath.Length;
 
-
+  private static string worldName = null; // name of the current world
 
   // returns name, date (in that order) on all saves
   public static List<SaveInfo> Fetch() {
@@ -40,7 +42,7 @@ public class DataService {
       SaveInfo saveInfo = new SaveInfo();
       
       saveInfo.path = path;
-      saveInfo.name = path.Substring(savePathLen);
+      saveInfo.name = path.Substring(savePathLen); // TODO: save world name separately
       saveInfo.lastModified = Directory.GetLastWriteTime(path);
 
       Debug.Log("saveInfo.path: " + saveInfo.path);
@@ -55,9 +57,31 @@ public class DataService {
     return info;
   }
 
-  // saves current game into savePath, returns success through a boolean (TODO: saves into given path)
-  public static bool Save(string namedSavePath) {
+  // creates a new save file with the given name, returns new SaveInfo struct representing the new save file
+  // NOTE: will overwrite worlds with the same name
+  public static SaveInfo NewSave(string name) {
     Directory.CreateDirectory(savePath);
+
+    worldName = name;
+    SaveCurr();
+
+    SaveInfo saveInfo = new SaveInfo();
+    saveInfo.path = savePath + worldName;
+    saveInfo.name = worldName;
+    saveInfo.lastModified = Directory.GetLastWriteTime(saveInfo.path);
+
+    return saveInfo;
+  }
+
+  // saves current game into savePath + name, returns success through a boolean
+  public static bool SaveCurr() {
+    Directory.CreateDirectory(savePath);
+
+    // checks if a world is currently loaded
+    if (worldName == null) {
+      Debug.Log("ERROR: World not loaded!");
+      return false;
+    }
 
     // dungeon
     GameObject dungeonGrid = GameObject.Find("DungeonGrid");
@@ -70,6 +94,7 @@ public class DataService {
     Debug.Log("dungeonJSON: " + dungeonJSON);
 
     // writing to file
+    string namedSavePath = savePath + worldName;
     Debug.Log("Saving to \"" + namedSavePath + "\"...");
     File.WriteAllText(namedSavePath, dungeonJSON);
     
@@ -79,16 +104,17 @@ public class DataService {
   }
 
   // loads game at savePath, returns success through a boolean
-  public static bool Load(string namedSavePath) {
+  public static bool Load(SaveInfo saveInfo) {
+    String path = saveInfo.path;
 
-    if (!File.Exists(namedSavePath)) {
+    if (!File.Exists(path)) {
       Debug.Log("ERROR: \"" + savePath + "\" does not exist!"); 
       return false;
     }
 
     // reading from file
-    Debug.Log("Loading from \"" + namedSavePath + "\"...");
-    List<string> dungeonJsons = File.ReadLines(namedSavePath).ToList();
+    Debug.Log("Loading from \"" + path + "\"...");
+    List<string> dungeonJsons = File.ReadLines(path).ToList();
     Debug.Log("dungeonJsons: " + dungeonJsons);
 
     // dungeon
@@ -98,6 +124,7 @@ public class DataService {
     foreach (string json in dungeonJsons)
       dungeonRenderer.Load(json);
 
+    worldName = saveInfo.name; // currently loaded world name
     Debug.Log("Loaded!");
     
     return true;
@@ -105,9 +132,11 @@ public class DataService {
 
   // TODO: deletes game at given path, returns success through a boolean
   public static bool Delete(string path) {
-    return true;
+    return false;
   }
-  public static string GetSavePath() { return savePath; } // retuns the save directory, which ends with a \
+  
+  // retuns the save directory, which ends with a \
+  public static string GetSavePath() { return savePath; }
 
 }
 
