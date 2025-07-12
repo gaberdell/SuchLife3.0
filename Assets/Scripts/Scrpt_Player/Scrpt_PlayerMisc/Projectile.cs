42d6d8294c0a22c65e7ede850ac6cdfe3b034fe7
RafikiMwethuku <Rmwathuku@gmail.com>
2025-07-11 22:48:38 -0400

    Fixed Projectile Shooting Bug
    Fixed the projectile shooting bug that was causing arrows shot to the right side to go left (almost like a magnet). All arrows now launch to the cursor correctly.

--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerMisc/Projectile.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerMisc/Projectile.cs
@@ -4,24 +4,24 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
 
-    public float lifeTime;    //if we want a lifetime on arrows
+    // public float lifeTime;    //if we want a lifetime on arrows
 
-    public GameObject destroyEffect;
+    // public GameObject destroyEffect;
 
-    //if we want a lifetime on arrows
-    private void Start()
-    {
-        Invoke("DestroyProjectile", lifeTime);
-    }
+    // //if we want a lifetime on arrows
+    // private void Start()
+    // {
+    //     Invoke("DestroyProjectile", lifeTime);
+    // }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(transform.up * speed * Time.deltaTime);
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
 
-    void destroyProjectile(){
-        Instantiate(destroyEffect, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-    }
+    // void destroyProjectile(){
+    //     Instantiate(destroyEffect, transform.position, Quaternion.identity);
+    //     Destroy(gameObject);
+    // }
 }
