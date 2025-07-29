d631604dc6b1cb908b868dda2c1cf9660ef6015d
pitbull51067 <pitbull51067@yahoo.com>
2025-07-29 21:31:46 +0200

    Powerup Icon Fixes
    We decided to not attach the icon to the player but put it at the corner of the screen to make it look better

--- Assets/PlayerBuffUI.cs
+++ Assets/PlayerBuffUI.cs
@@ -1,39 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerBuffUI : MonoBehaviour
 {
-    public GameObject mgBoostIcon;
-    public Transform playerHeadTransform;  // This is where the icon should hover (e.g., player's head or chest)
+    public GameObject mgBoostIcon;  // Drag the MgBoostIcon GameObject here in Inspector
 
-    private Camera mainCam;
-
-    void Start()
+    private void Start()
     {
-        mainCam = Camera.main;
         if (mgBoostIcon != null)
-            mgBoostIcon.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (mgBoostIcon != null && mgBoostIcon.activeSelf && playerHeadTransform != null)
-        {
-            // Convert world position to screen position
-            Vector3 screenPos = mainCam.WorldToScreenPoint(playerHeadTransform.position + Vector3.up * 1.5f);
-            mgBoostIcon.transform.position = screenPos;
-        }
+            mgBoostIcon.SetActive(false);  // Hide on start
     }
 
     public void ShowMgBoostIcon(float duration)
     {
-        if (mgBoostIcon != null)
-        {
-            mgBoostIcon.SetActive(true);
-            StartCoroutine(HideAfterDelay(duration));
-        }
+        if (mgBoostIcon == null) return;
+
+        mgBoostIcon.SetActive(true);
+        StartCoroutine(HideAfterDelay(duration));
     }
 
-    private System.Collections.IEnumerator HideAfterDelay(float duration)
+    private IEnumerator HideAfterDelay(float duration)
     {
         yield return new WaitForSeconds(duration);
         if (mgBoostIcon != null)
