ea30cb7877ab4edeec16273159ed14dc2dd7de84
RafikiMwethuku <Rmwathuku@gmail.com>
2025-08-12 05:28:36 -0400

    Finalized Crossbow Charging Logic and Sprite Changing
    Crossbow charging is now polished and working. Added sprite changing to the charging logic. If the mouse if held for half of the full charge time, it switches to a half charged sprite. If it is released before fully charging, it returns to the no charge sprite. If the mouse is held for the duration of the full charge time, crossbow enters charge state and stays in that state until the user clicks again to shoot an arrow.

--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerMisc/Crossbow.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerMisc/Crossbow.cs
@@ -9,16 +9,24 @@ public class Crossbow : MonoBehaviour
     public GameObject projectile;
     public Transform shotPoint;
 
-    private float timeBtwShots;
-    public float startTimeBtwShots;
-
     public float timeToFullCharge;
     private float timeHeld;
+    private float holdStartTime;
+    private bool isHolding;
     private bool isCharged;
 
     //3 different states of transition for crossbow charge
+    public Sprite idleSprite;
+    public Sprite chargingSprite;
+    public Sprite chargedSprite;
 
+    private SpriteRenderer sr;
 
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        if (sr != null && idleSprite != null) sr.sprite = idleSprite;
+    }
 
     // Update is called once per frame
     private void Update()
@@ -28,24 +36,60 @@ public class Crossbow : MonoBehaviour
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
-        if(Input.GetMouseButton(0)){
-            timeHeld += Time.deltaTime;
-        }
-
-        if(timeHeld >= timeToFullCharge){
-            isCharged = true;
-        }
-
-        if(timeBtwShots <= 0 && isCharged){
-            if(Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0)) //if mouse button is pressed at all
+        {
+            //if crossbow already fully charged, fire on this click
+            if (isCharged)
+            {
                 Instantiate(projectile, shotPoint.position, transform.rotation);
-                timeBtwShots = startTimeBtwShots;
+
                 isCharged = false;
-                timeHeld = 0f;
+                if (sr && idleSprite) sr.sprite = idleSprite;
+            }
+            else //if crossbow isn't fully charged
+            {
+                isHolding = true;
+                isCharged = false;
+                holdStartTime = Time.time;
+                if (sr && idleSprite) sr.sprite = idleSprite;
             }
         }
-        else{
-            timeBtwShots -= Time.deltaTime;
+
+
+        if (Input.GetMouseButton(0) && isHolding) //if mouse button is being held
+        {
+            float held = Time.time - holdStartTime;
+
+            if (held >= timeToFullCharge)
+            {
+                isCharged = true;
+                if (sr && chargedSprite) sr.sprite = chargedSprite;
+            }
+            else if (held >= timeToFullCharge * 0.5f)
+            {
+                if (sr && chargingSprite) sr.sprite = chargingSprite;
+            }
+            else
+            {
+                if (sr && idleSprite) sr.sprite = idleSprite;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && isHolding) //if mouse button is released 
+        {
+            isHolding = false;
+
+            //if released before full charge, reset to idle
+            if (!isCharged && sr && idleSprite) sr.sprite = idleSprite;
+
+        }
+
+        if (isCharged && Input.GetMouseButtonDown(0)) //if crossbow fully charged and mouse is clicked
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+
+            isCharged = false;
+            if (sr && idleSprite) sr.sprite = idleSprite;
         }
 
     }
