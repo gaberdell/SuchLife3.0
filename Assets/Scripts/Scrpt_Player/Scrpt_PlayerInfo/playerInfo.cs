e23ad7a077d335e309f5a6cdadd943bc3774538b
colekrushel <krushc@rpi.edu>
2026-03-20 17:33:22 -0400

    Fixed held items for playerobjects
    playerinfo and associated held item handling now works on the player object only

--- Assets/Scripts/Scrpt_Player/Scrpt_PlayerInfo/playerInfo.cs
+++ Assets/Scripts/Scrpt_Player/Scrpt_PlayerInfo/playerInfo.cs
@@ -10,9 +10,13 @@ public class PlayerInfo : MonoBehaviour
     public GameObject Player { get; private set; }
     private string username;
     private string heldType;
-    private Item heldItem;
+    private Item _heldItem;
     public int heldItemIndex = 0;
-    public PlayerItemHoldView heldItemView;
+    //public PlayerItemHoldView heldItemView;
+    [SerializeField] SpriteRenderer currentlyHeld; //currently held item
+
+    public Item CurrentlyHeldItem;
+
 
     private void Start()
     {
@@ -23,15 +27,30 @@ public class PlayerInfo : MonoBehaviour
     {
         get
         {
-            return heldItem;
+            return _heldItem;
         }
 
         set
         {
             //Debug.Log("change held item!");
-            heldItem = value;
+            _heldItem = value;
             //when held item is changed, change the renderer accordingly
-            heldItemView.playerHandUpdate(heldItem);
+            playerHandUpdate(_heldItem);
+        }
+    }
+
+    private void playerHandUpdate(Item newItem)
+    {
+        CurrentlyHeldItem = newItem;
+
+        //switch rotation when that gets added
+        if (newItem != null)
+        {
+            currentlyHeld.sprite = CurrentlyHeldItem.icon;
+        }
+        else
+        {
+            currentlyHeld.sprite = null;
         }
     }
 
